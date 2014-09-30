9b72ac3503b4b2749b3628e3bf1df3f5fcdc3ba4
Ivan Rodriguez <irodriguez@rocketlawyer.com>
2014-09-29 19:31:10 -0700

    Logging in to parse

--- ChatClient/ViewController.swift
+++ ChatClient/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailInputField: UITextField!
+    @IBOutlet weak var passwordInputField: UITextField!
+    
+    var email: String?
+    var password: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,42 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func signUp(sender: AnyObject) {
+        email = emailInputField.text ?? ""
+        password = passwordInputField.text ?? ""
+        
+        var user = PFUser()
+        user.username = email
+        user.password = password
+        user.email = email
+        
+        user.signUpInBackgroundWithBlock {
+            (succeeded: Bool!, error: NSError!) -> Void in
+            if error == nil {
+                println("We signed up")
+                return
+            } else {
+//                let errorString = error.userInfo["error"] as NSString
+                println(error)
+            }
+        }
+        
+    }
+
+    @IBAction func signIn(sender: AnyObject) {
+        println("Inside function")
+        email = emailInputField.text ?? ""
+        password = passwordInputField.text ?? ""
+        
+        PFUser.logInWithUsernameInBackground(self.emailInputField.text, password: self.passwordInputField.text) {
+            (user: PFUser!, error: NSError!) -> Void in
+            if user != nil {
+                println("Signed Up")
+            } else {
+                println("Didn't work to signup")
+            }
+        }
+    }
 
 }
 
